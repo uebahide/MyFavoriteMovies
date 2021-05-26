65dc4bbdff8d4a6051e33be6d4f495012bba2edf
uebahide <uebahide@gmail.com>
2021-05-26 16:27:16 +0900

    add MobileMenu

---
+++ Modules/MobileMenu/main.js
@@ -0,0 +1,25 @@
+class MobileMenu{
+  constructor(){
+    this.DOM = {},
+    this.DOM.btn = document.querySelector('.mobile-menu__btn'),
+    this.DOM.cover = document.querySelector('.mobile-menu__cover'),
+    this.DOM.container = document.querySelector('#global-container'),
+    this.eventType = this._getEventType();
+    this._addEvent()
+  }
+  
+  _toggle(){
+    this.DOM.container.classList.toggle('inview');
+  }
+  
+  _addEvent(){
+    this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+    this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
+  }
+
+  _getEventType(){
+    return window.ontouchstart ? 'touchstart' : 'click';
+  }
+}
+
+new MobileMenu();

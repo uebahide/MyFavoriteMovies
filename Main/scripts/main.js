bc7ae4be435aa0e041f656015c373feb38ce4166
uebahide <uebahide@gmail.com>
2021-05-30 10:31:40 +0900

    add Category(integrater FadeImage into master)

--- Main/scripts/main.js
+++ Main/scripts/main.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded', ()=>{
   new MobileMenu();
+
+
+  const SlideImage = document.querySelectorAll('.fade-img');
+
+  const callback = function(entries, observer){
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('inview')
+      }else{
+        // entry.target.classList.remove('inview');
+      }
+    });
+  }
+
+  const options = {
+    root: null,
+    rootMargin: '0px 0px',
+    threshold: 0
+  }
+
+  const Observer = new IntersectionObserver(callback, options);
+
+  SlideImage.forEach(el => {
+    Observer.observe(el);
+  });
 })

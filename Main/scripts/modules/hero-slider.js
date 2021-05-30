53fcbea5e77c204da75495983bff735263f01ac8
uebahide <uebahide@gmail.com>
2021-05-30 11:02:33 +0900

    add some functionallity to HeroSlider

--- Main/scripts/modules/hero-slider.js
+++ Main/scripts/modules/hero-slider.js
@@ -10,21 +10,5 @@ const swiper = new Swiper('.swiper-container', {
     1024:{
       slidesPerView: 2
     }
-  },
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  }
 });

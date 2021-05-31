6342f314ce9cb04fdb18acac2c5f0c3dcd41a530
uebahide <uebahide@gmail.com>
2021-05-31 17:07:23 +0900

    add SliderClass, Information Section, and add some changes

--- Main/scripts/main.js
+++ Main/scripts/main.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', ()=>{
   new MobileMenu();
+  const heroSlider = new Slider('.swiper-container');
+  const informationSlider = new Slider('.information-swiper-container',{
+    effect: 'slide',
+    centeredSlides: false,
+    slidesPerView: 2,
+    breakpoints: {
+      600:{
+        slidesPerView: 3
+      },
 
+      960:{
+        slidesPerView: 4
+      }
+    }
+  });
+  informationSlider.Start();
 
   const fadeImage = function(el, inview){
       if(inview){
@@ -11,5 +26,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
   };
   const so1 = new ScrollObserver('.fade-img', fadeImage);
 
-  
+
+  const header = document.querySelector('.header');
+  const navAnimation = function(el, inview){
+      if(inview){
+        header.classList.remove('triggered');
+      }else{
+        header.classList.add('triggered');
+      }
+  };
+  const so2 = new ScrollObserver('.nav-trigger', navAnimation, {once: false});
 })
--- Main/scripts/modules/hero-slider.js
+++ Main/scripts/modules/hero-slider.js
@@ -1,14 +1,38 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  grabCursor: true,
-  effect: 'coverflow',
-  centeredSlides: true,
-  slidesPerView: 1,
-  breakpoints:{
-    1024:{
-      slidesPerView: 2
-    }
+class Slider{
+  constructor(el, options){
+    this.el = el,
+    this.defaultOptions = {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      grabCursor: true,
+      effect: 'coverflow',
+      centeredSlides: true,
+      slidesPerView: 1,
+      breakpoints:{
+        1024:{
+          slidesPerView: 2
+        }
+      }
+    },
+    this.options = Object.assign(this.defaultOptions, options),
+    this.Swiper= this._initSwiper()
   }
-});
+
+  _initSwiper(){
+    return new Swiper(this.el, this.options);
+  }
+
+  Start(options={}){
+    options = Object.assign({
+      delay: 4000,
+      disableOnInteraction: false,//マウスで操作した後もautoplayが消えない
+    }, options);
+    this.Swiper.params.autoplay = options;
+    this.Swiper.autoplay.start();
+  }
+
+  Stop(){
+    this.Swiper.autoplay.stop();
+  }
+}
